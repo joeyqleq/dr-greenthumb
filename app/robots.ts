f40f59db0a5d3367bc92135c2cd1160ba1bbdb8b
joeyq <joemaari@gmail.com>
2026-06-22 12:51:00 +0300

    Create robots.txt for SEO

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/.next/'],
+    },
+    sitemap: 'https://v0-dr-greenthumb.vercel.app/sitemap.xml',
+  }
+}
